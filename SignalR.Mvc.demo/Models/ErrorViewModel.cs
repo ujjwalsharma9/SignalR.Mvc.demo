2bb8ea3fba68c1fcba693f9bb32826a505754f15
Ujjwal Sharma <Ujjwal.Sharma@compunneldigital.com>
2020-08-23 12:41:57 +0530

    Add project files.

---
+++ SignalR.Mvc.demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SignalR.Mvc.demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
